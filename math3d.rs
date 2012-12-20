b9279e8eb1aa374e54fdd9ebb4128b3e8d6d2490
Matthijs 'Thiez' Hofstra <thiezz@gmail.com>
2012-12-20 16:00:40 +0100

    Lots of changes, lost history in a file transfer, stuff should work with Rust 0.5?

--- math3d.rs
+++ math3d.rs
@@ -1,88 +1,88 @@
 pub struct vec3 { x:f32, y:f32, z:f32 }
 
 impl vec3: cmp::Eq {
-    pure fn eq(other: &vec3) -> bool {
-        self.x == other.x && self.y == other.y && self.z == other.z
-    }
+	pure fn eq(&self, other: &vec3) -> bool {
+		self.x == other.x && self.y == other.y && self.z == other.z
+	}
 
-    pure fn ne(other: &vec3) -> bool {
-        !self.eq(other)
-    }
+	pure fn ne(&self, other: &vec3) -> bool {
+		!self.eq(other)
+	}
 }
 
 pub type mtx33 = { r0:vec3, r1:vec3, r2:vec3 };
 
 #[inline(always)]
 pub fn vec3(x:f32, y:f32, z:f32) -> vec3{
-    vec3 {x:x, y:y, z:z}
+	vec3 {x:x, y:y, z:z}
 }
 
 #[inline(always)]
 pub fn dot(a:vec3, b:vec3) -> f32 {
-    a.x*b.x + a.y*b.y + a.z*b.z
+	a.x*b.x + a.y*b.y + a.z*b.z
 }
 
 #[inline(always)]
 pub fn lerp(a:vec3, b:vec3, t:f32) -> vec3{
-    add( a, scale(sub(b,a),t) )
+	add( a, scale(sub(b,a),t) )
 }
 
 #[inline(always)]
 pub fn scale(v:vec3, s:f32) -> vec3 {
-    vec3 { x:v.x*s, y:v.y*s, z:v.z*s }
+	vec3 { x:v.x*s, y:v.y*s, z:v.z*s }
 }
 
 #[inline(always)]
 pub fn length_sq(v:vec3) -> f32 {
-    dot(v,v)
+	dot(v,v)
 }
 
 #[inline(always)]
 pub fn length(v:vec3) -> f32 {
-    f32::sqrt(length_sq(v))
+	f32::sqrt(length_sq(v))
 }
 
 #[inline(always)]
 pub fn normalized(v:vec3) -> vec3 {
-    scale(v, 1.0f32 / length(v))
+	scale(v, 1.0f32 / length(v))
 }
 
 #[inline(always)]
 pub fn recip(a:vec3) -> vec3{
-    vec3(1f32/a.x, 1f32/a.y, 1f32/a.z)
+	vec3(1f32/a.x, 1f32/a.y, 1f32/a.z)
 }
 
 
 #[inline(always)]
 pub fn mul(a:vec3, b:vec3) -> vec3{
-    vec3(a.x*b.x, a.y*b.y, a.z*b.z)
+	vec3(a.x*b.x, a.y*b.y, a.z*b.z)
 }
 
 #[inline(always)]
 pub fn add(a:vec3, b:vec3) -> vec3 {
-    vec3 {x:a.x+b.x, y:a.y+b.y, z:a.z+b.z}
+	vec3 {x:a.x+b.x, y:a.y+b.y, z:a.z+b.z}
 }
 
 #[inline(always)]
 pub fn sub(a:vec3, b:vec3) -> vec3 {
-    add(a, scale(b, -1.0f32))
+	add(a, scale(b, -1.0f32))
 }
 
 #[inline(always)]
 pub fn cross(a:vec3, b:vec3) -> vec3 {
-    vec3 { x: a.y*b.z - b.y*a.z,
-          y: a.z*b.x - b.z*a.x,
-          z: a.x*b.y - b.x*a.y }
+	vec3 { x: a.y*b.z - b.y*a.z,
+		  y: a.z*b.x - b.z*a.x,
+		  z: a.x*b.y - b.x*a.y }
 }
 
 #[inline(always)]
 pub fn min(a: vec3, b: vec3) -> vec3 {
-    vec3( f32::fmin(a.x,b.x), f32::fmin(a.y, b.y), f32::fmin(a.z, b.z) )
+	vec3( f32::fmin(a.x,b.x), f32::fmin(a.y, b.y), f32::fmin(a.z, b.z) )
 }
 
 #[inline(always)]
 pub fn max(a: vec3, b: vec3) -> vec3 {
-    vec3( f32::fmax(a.x,b.x), f32::fmax(a.y, b.y), f32::fmax(a.z, b.z) )
+	vec3( f32::fmax(a.x,b.x), f32::fmax(a.y, b.y), f32::fmax(a.z, b.z) )
 }
 
 pub type ray = { origin:vec3, dir:vec3 };
@@ -93,114 +93,114 @@ pub type hit_result = { barycentric: vec3, t: f32 };
 
 #[inline(always)]
 pub fn ray_triangle_intersect( r:ray, t:triangle ) -> Option<hit_result> {
-    let e1 = sub(t.p2, t.p1);
-    let e2 = sub(t.p3, t.p1);
-    let s1 = cross(r.dir, e2);
-    let divisor = dot(s1,e1);
+	let e1 = sub(t.p2, t.p1);
+	let e2 = sub(t.p3, t.p1);
+	let s1 = cross(r.dir, e2);
+	let divisor = dot(s1,e1);
 
-    if divisor == 0f32 {
-        return option::None;
-    }
+	if divisor == 0f32 {
+		return option::None;
+	}
 
-    // compute first barycentric coordinate
-    let inv_divisor = 1.0f32 / divisor;
-    let d = sub(r.origin, t.p1);
+	// compute first barycentric coordinate
+	let inv_divisor = 1.0f32 / divisor;
+	let d = sub(r.origin, t.p1);
 
-    let b1 = dot(d, s1) * inv_divisor;
-    if b1 < 0.0f32 || b1 > 1.0f32 {
-        return option::None; // outside triangle
-    }
+	let b1 = dot(d, s1) * inv_divisor;
+	if b1 < 0.0f32 || b1 > 1.0f32 {
+		return option::None; // outside triangle
+	}
 
-    // and second barycentric coordinate
-    let s2 = cross(d, e1);
-    let b2 = dot(r.dir, s2)*inv_divisor;
-    if b2 < 0.0f32 || b1+b2 > 1.0f32 {
-        return option::None; // outside triangle
-    }
+	// and second barycentric coordinate
+	let s2 = cross(d, e1);
+	let b2 = dot(r.dir, s2)*inv_divisor;
+	if b2 < 0.0f32 || b1+b2 > 1.0f32 {
+		return option::None; // outside triangle
+	}
 
-    let t = dot(e2, s2) * inv_divisor;
-    if t < 0.0f32 {
-        return option::None; // behind viewer
-    }
+	let t = dot(e2, s2) * inv_divisor;
+	if t < 0.0f32 {
+		return option::None; // behind viewer
+	}
 
-    option::Some( {barycentric: vec3 { x:b1, y: b2, z: 1.0f32-b1-b2 }, t: t} )
+	option::Some( {barycentric: vec3 { x:b1, y: b2, z: 1.0f32-b1-b2 }, t: t} )
 }
 
 pub type aabb = { min: vec3, max: vec3 };
 
 #[inline(always)]
 pub fn ray_aabb_check( r:ray, max_dist: f32, box: aabb ) -> bool {
-    let inv_dir = recip(r.dir);
-    let (tx1,tx2,ty1,ty2,tz1,tz2) = (
-        (box.min.x - r.origin.x)*inv_dir.x,
-        (box.max.x - r.origin.x)*inv_dir.x,
-        (box.min.y - r.origin.y)*inv_dir.y,
-        (box.max.y - r.origin.y)*inv_dir.y,
-        (box.min.z - r.origin.z)*inv_dir.z,
-        (box.max.z - r.origin.z)*inv_dir.z
-    );
+	let inv_dir = recip(r.dir);
+	let (tx1,tx2,ty1,ty2,tz1,tz2) = (
+		(box.min.x - r.origin.x)*inv_dir.x,
+		(box.max.x - r.origin.x)*inv_dir.x,
+		(box.min.y - r.origin.y)*inv_dir.y,
+		(box.max.y - r.origin.y)*inv_dir.y,
+		(box.min.z - r.origin.z)*inv_dir.z,
+		(box.max.z - r.origin.z)*inv_dir.z
+	);
 
-    let (minx, maxx) = (f32::fmin(tx1,tx2), f32::fmax(tx1,tx2));
-    let (miny, maxy) = (f32::fmin(ty1,ty2), f32::fmax(ty1,ty2));
-    let (minz, maxz) = (f32::fmin(tz1,tz2), f32::fmax(tz1,tz2));
+	let (minx, maxx) = (f32::fmin(tx1,tx2), f32::fmax(tx1,tx2));
+	let (miny, maxy) = (f32::fmin(ty1,ty2), f32::fmax(ty1,ty2));
+	let (minz, maxz) = (f32::fmin(tz1,tz2), f32::fmax(tz1,tz2));
 
-    let tmin = f32::fmax(minx, f32::fmax(miny, minz));
-    let tmax = f32::fmin(maxx, f32::fmin(maxy, maxz));
+	let tmin = f32::fmax(minx, f32::fmax(miny, minz));
+	let tmax = f32::fmin(maxx, f32::fmin(maxy, maxz));
 
-    tmax >= 0f32 && tmin <= tmax && tmin <= max_dist
+	tmax >= 0f32 && tmin <= tmax && tmin <= max_dist
 }
 
 // Gives a cosine hemisphere sample from two uniform f32s
 // in [0,1) range.
 #[inline(always)]
 pub fn cosine_hemisphere_sample( u: f32, v: f32 ) -> vec3 {
-    let r_sqrt = f32::sqrt(u);
-    let theta = 2f32 * f32::consts::pi * v;
-    vec3( r_sqrt*f32::cos(theta), f32::sqrt(1f32-u), r_sqrt*f32::sin(theta))
+	let r_sqrt = f32::sqrt(u);
+	let theta = 2f32 * f32::consts::pi * v;
+	vec3( r_sqrt*f32::cos(theta), f32::sqrt(1f32-u), r_sqrt*f32::sin(theta))
 }
 
 #[inline(always)]
 pub fn rotate_to_up( up_vec: vec3 ) -> mtx33 {
-    let perp = if up_vec == vec3(0f32,1f32,0f32) { vec3(1f32,0f32,0f32) } else { vec3(0f32,1f32,0f32) };
-    let right = cross( up_vec, perp );
-    let fwd = cross(right, up_vec );
-    transposed( { r0: right, r1: up_vec, r2: fwd } )
+	let perp = if up_vec == vec3(0f32,1f32,0f32) { vec3(1f32,0f32,0f32) } else { vec3(0f32,1f32,0f32) };
+	let right = cross( up_vec, perp );
+	let fwd = cross(right, up_vec );
+	transposed( { r0: right, r1: up_vec, r2: fwd } )
 }
 
 #[inline(always)]
 pub fn rotate_y(theta: f32) -> mtx33{
-    let ct = f32::cos(theta);
-    let st = f32::sin(theta);
-    { r0: vec3(ct,0f32,st), r1: vec3(0f32,1f32,0f32), r2: vec3(-st, 0f32, ct) }
+	let ct = f32::cos(theta);
+	let st = f32::sin(theta);
+	{ r0: vec3(ct,0f32,st), r1: vec3(0f32,1f32,0f32), r2: vec3(-st, 0f32, ct) }
 }
 
 #[inline(always)]
 pub fn transform( m: mtx33, v: vec3 ) -> vec3 {
-    vec3( dot( m.r0, v ), dot( m.r1, v ), dot( m.r2, v ) )
+	vec3( dot( m.r0, v ), dot( m.r1, v ), dot( m.r2, v ) )
 }
 
 #[inline(always)]
 pub fn mul_mtx33( a: mtx33, b: mtx33 ) -> mtx33 {
-    let b_t = transposed(b);
-    {   r0: vec3( dot(a.r0,b_t.r0), dot(a.r0,b_t.r1), dot(a.r0,b_t.r2) ),
-        r1: vec3( dot(a.r1,b_t.r0), dot(a.r1,b_t.r1), dot(a.r1,b_t.r2) ),
-        r2: vec3( dot(a.r2,b_t.r0), dot(a.r2,b_t.r1), dot(a.r2,b_t.r2) ) }
+	let b_t = transposed(b);
+	{   r0: vec3( dot(a.r0,b_t.r0), dot(a.r0,b_t.r1), dot(a.r0,b_t.r2) ),
+		r1: vec3( dot(a.r1,b_t.r0), dot(a.r1,b_t.r1), dot(a.r1,b_t.r2) ),
+		r2: vec3( dot(a.r2,b_t.r0), dot(a.r2,b_t.r1), dot(a.r2,b_t.r2) ) }
 }
 
 #[inline(always)]
 pub fn transposed( m: mtx33 ) -> mtx33 {
-    {   r0: vec3( m.r0.x, m.r1.x, m.r2.x ),
-        r1: vec3( m.r0.y, m.r1.y, m.r2.y ),
-        r2: vec3( m.r0.z, m.r1.z, m.r2.z ) }
+	{   r0: vec3( m.r0.x, m.r1.x, m.r2.x ),
+		r1: vec3( m.r0.y, m.r1.y, m.r2.y ),
+		r2: vec3( m.r0.z, m.r1.z, m.r2.z ) }
 }
 
 #[test]
 pub fn intersection_test()
 {
-    let ray : ray = { origin: vec3(0f32, 0f32, 0f32), dir: vec3(0.0f32,0.0f32,-1.0f32) };
-    let tri : triangle = {  p1: vec3(-1f32, -1f32, -1f32),
-                    p2: vec3(1f32, -1f32, -1f32),
-                    p3: vec3(0f32, 2f32, -1f32) };
+	let ray : ray = { origin: vec3(0f32, 0f32, 0f32), dir: vec3(0.0f32,0.0f32,-1.0f32) };
+	let tri : triangle = {  p1: vec3(-1f32, -1f32, -1f32),
+					p2: vec3(1f32, -1f32, -1f32),
+					p3: vec3(0f32, 2f32, -1f32) };
 
-    assert option::is_some( &ray_triangle_intersect( ray, tri ) );
+	assert option::is_some( &ray_triangle_intersect( ray, tri ) );
 }
--- model.rs
+++ model.rs
@@ -7,345 +7,339 @@ pub type polysoup = { vertices: ~[vec3], indices: ~[uint], normals: ~[vec3] };
 pub type mesh = { polys: polysoup, kd_tree: kd_tree, bounding_box: aabb };
 
 pub enum axis {
-    pub x,
-    pub y,
-    pub z
+	pub x,
+	pub y,
+	pub z
 }
 
-pub type kd_tree = { nodes : ~[kd_tree_node], root: uint };
+pub struct kd_tree {
+	root: uint,
+	nodes: ~[kd_tree_node]
+}
 
 pub enum kd_tree_node {
-    pub leaf( u32, u32 ),
-    pub node( axis, f32, u32 )
+	pub leaf( u32, u32 ),
+	pub node( axis, f32, u32 )
 }
 
 fn find_split_plane( distances: &[f32], indices: &[uint], faces: &[uint] ) -> f32 {
 
-    let mut face_distances = ~[];
-    for faces.each |f| {
-        face_distances.push(distances[indices[*f*3u]]);
-        face_distances.push(distances[indices[*f*3u+1u]]);
-        face_distances.push(distances[indices[*f*3u+2u]]);
-    }
+	let mut face_distances = ~[];
+	for faces.each |f| {
+		face_distances.push(distances[indices[*f*3u]]);
+		face_distances.push(distances[indices[*f*3u+1u]]);
+		face_distances.push(distances[indices[*f*3u+2u]]);
+	}
 
-    let mut sorted_distances = sort::merge_sort( |a,b| *a<*b, face_distances );
-    let n = vec::len(sorted_distances);
-    if n % 2u == 0u {
-        sorted_distances[ n/2u ]
-    } else {
-        (sorted_distances[ n/2u -1u] + sorted_distances[ n/2u ]) * 0.5f32
-    }
+	let mut sorted_distances = sort::merge_sort( face_distances, |a,b| *a<*b );
+	let n = vec::len(sorted_distances);
+	if n % 2u == 0u {
+		sorted_distances[ n/2u ]
+	} else {
+		(sorted_distances[ n/2u -1u] + sorted_distances[ n/2u ]) * 0.5f32
+	}
 }
 
 fn split_triangles( splitter: f32, distances: &[f32], indices: &[uint], faces: &[uint] ) -> {l:~[uint],r:~[uint]} {
-    let mut l = ~[];
-    let mut r = ~[];
+	let mut l = ~[];
+	let mut r = ~[];
 
-    for faces.each |f| {
-        let f = *f;
-        let d0 = distances[indices[f*3u   ]];
-        let d1 = distances[indices[f*3u+1u]];
-        let d2 = distances[indices[f*3u+2u]];
+	for faces.each |f| {
+		let f = *f;
+		let d0 = distances[indices[f*3u   ]];
+		let d1 = distances[indices[f*3u+1u]];
+		let d2 = distances[indices[f*3u+2u]];
 
-        let maxdist = f32::fmax(d0, f32::fmax(d1, d2));
-        let mindist = f32::fmin(d0, f32::fmin(d1, d2));
+		let maxdist = f32::fmax(d0, f32::fmax(d1, d2));
+		let mindist = f32::fmin(d0, f32::fmin(d1, d2));
 
-        if mindist <= splitter {
-            l += [f];
-        }
+		if mindist <= splitter {
+			l += [f];
+		}
 
-        if maxdist >= splitter {
-            r += [f];
-        }
-    }
+		if maxdist >= splitter {
+			r += [f];
+		}
+	}
 
-    {l:l, r:r}
+	{l:move l, r:move r}
 }
 
 fn build_leaf(
-    kd_tree_nodes : &mut ~[kd_tree_node],
-    new_indices: &mut ~[uint],
-    indices: &[uint],
-    faces: &[uint]
-) -> uint {
+	kd_tree_nodes : &mut ~[kd_tree_node],
+	new_indices: &mut ~[uint],
+	indices: &[uint],
+	faces: &[uint]
+	) -> uint {
 
-    let next_face_ix : u32 = (new_indices.len() as u32) / 3u32;
-    kd_tree_nodes.push(leaf( next_face_ix, (vec::len(faces) as u32) ));
+	let next_face_ix : u32 = (new_indices.len() as u32) / 3u32;
+	kd_tree_nodes.push(leaf( next_face_ix, (vec::len(faces) as u32) ));
 
-    for faces.each |f| {
-        let f = *f;
-        *new_indices += &[ indices[f*3u], indices[f*3u+1u], indices[f*3u+2u] ];
-    }
-    return kd_tree_nodes.len() - 1u;
+	for faces.each |f| {
+		let f = *f;
+		*new_indices += &[ indices[f*3u], indices[f*3u+1u], indices[f*3u+2u] ];
+	}
+	kd_tree_nodes.len() - 1u
 }
 
 fn build_kd_tree(
-    kd_tree_nodes : &mut ~[kd_tree_node],
-    new_indices: &mut ~[uint],
-    maxdepth: uint,
-    xdists: &r/[f32],
-    ydists: &r/[f32],
-    zdists: &r/[f32],
-    aabbmin: vec3,
-    aabbmax: vec3,
-    indices: &[uint],
-    faces: &[uint] ) -> uint {
+	kd_tree_nodes : &mut ~[kd_tree_node],
+	new_indices: &mut ~[uint],
+	maxdepth: uint,
+	xdists: &r/[f32],
+	ydists: &r/[f32],
+	zdists: &r/[f32],
+	aabbmin: vec3,
+	aabbmax: vec3,
+	indices: &[uint],
+	faces: &[uint] ) -> uint {
 
-    if maxdepth == 0u || vec::len(faces) <= 15u {
-        return build_leaf( kd_tree_nodes, new_indices, indices, faces );
-    }
+	if maxdepth == 0u || vec::len(faces) <= 15u {
+		return build_leaf( kd_tree_nodes, new_indices, indices, faces );
+	}
 
-    let extent = sub(aabbmax, aabbmin);
-        let axis = if extent.x > extent.y && extent.x > extent.z {
-            x
-        } else {
-            if extent.y > extent.z { y } else { z }
-        };
+	let extent = sub(aabbmax, aabbmin);
+		let axis = if extent.x > extent.y && extent.x > extent.z {
+			x
+		} else {
+			if extent.y > extent.z { y } else { z }
+		};
 
-    let mut dists;
-    match axis {
-        x() => { dists = xdists }
-        y() => { dists = ydists }
-        z() => { dists = zdists }
-    };
+	let mut dists;
+	match axis {
+		x => { dists = xdists }
+		y => { dists = ydists }
+		z => { dists = zdists }
+	};
 
-    let s = find_split_plane( dists, indices, faces );
-    let {l,r} = split_triangles( s, dists, indices, faces );
+	let s = find_split_plane( dists, indices, faces );
+	let {l,r} = split_triangles( s, dists, indices, faces );
 
-    // Stop when there's too much overlap between the two halves
-    if (vec::len(l) + vec::len(r)) as f32 > vec::len(faces) as f32 * 1.5f32 {
-        return build_leaf( kd_tree_nodes, new_indices, indices, faces );
-    }
+	// Stop when there's too much overlap between the two halves
+	if (vec::len(l) + vec::len(r)) as f32 > vec::len(faces) as f32 * 1.5f32 {
+		return build_leaf( kd_tree_nodes, new_indices, indices, faces );
+	}
 
-    // adjust bounding boxes for children
-    let mut left_aabbmax;
-    let mut right_aabbmin;
-    match axis {
-        x() => {
-            left_aabbmax = vec3 { x: s, .. aabbmax };
-            right_aabbmin = vec3 { x: s, .. aabbmin };
-        }
-        y() => {
-            left_aabbmax = vec3 { y: s, .. aabbmax };
-            right_aabbmin = vec3 { y: s, .. aabbmin };
-        }
-        z() => {
-            left_aabbmax = vec3 { z: s, .. aabbmax };
-            right_aabbmin = vec3 { z: s, .. aabbmin };
-        }
-    };
+	// adjust bounding boxes for children
+	let mut left_aabbmax;
+	let mut right_aabbmin;
+	match axis {
+		x() => {
+			left_aabbmax = vec3 { x: s, .. aabbmax };
+			right_aabbmin = vec3 { x: s, .. aabbmin };
+		}
+		y() => {
+			left_aabbmax = vec3 { y: s, .. aabbmax };
+			right_aabbmin = vec3 { y: s, .. aabbmin };
+		}
+		z() => {
+			left_aabbmax = vec3 { z: s, .. aabbmax };
+			right_aabbmin = vec3 { z: s, .. aabbmin };
+		}
+	};
 
-    // allocate node from nodes-array, and recursively build children
-    let ix = kd_tree_nodes.len();
-    vec::push( kd_tree_nodes, node(axis,0f32,0u32));
+	// allocate node from nodes-array, and recursively build children
+	let ix = kd_tree_nodes.len();
+	vec::push( kd_tree_nodes, node(axis,0f32,0u32));
 
-    build_kd_tree(
-        kd_tree_nodes,
-        new_indices,
-        maxdepth - 1u,
-        xdists,
-        ydists,
-        zdists,
-        aabbmin,
-        left_aabbmax,
-        indices,
-        l
-    );
-    // left child ix is implied to be ix+1
+	build_kd_tree(
+		kd_tree_nodes,
+		new_indices,
+		maxdepth - 1u,
+		xdists,
+		ydists,
+		zdists,
+		aabbmin,
+		left_aabbmax,
+		indices,
+		l
+	);
+	// left child ix is implied to be ix+1
 
-    let right_child_ix = build_kd_tree(
-        kd_tree_nodes,
-        new_indices,
-        maxdepth - 1u,
-        xdists,
-        ydists,
-        zdists,
-        right_aabbmin,
-        aabbmax,
-        indices,
-        r
-    );
+	let right_child_ix = build_kd_tree(
+		kd_tree_nodes,
+		new_indices,
+		maxdepth - 1u,
+		xdists,
+		ydists,
+		zdists,
+		right_aabbmin,
+		aabbmax,
+		indices,
+		r
+	);
 
-    kd_tree_nodes[ix] = node(axis, s as f32, right_child_ix as u32);
+	kd_tree_nodes[ix] = node(axis, s as f32, right_child_ix as u32);
 
-    return ix;
+	return ix;
 }
 
-pub fn count_kd_tree_nodes( t: kd_tree ) -> {depth:uint, count:uint} {
-    match t.nodes[t.root] {
-        node(_,_,r) => {
-            let {depth:d0,count:c0} = count_kd_tree_nodes( {root: t.root+1u, .. t} );
-            let {depth:d1,count:c1} = count_kd_tree_nodes( {root: (r as uint), .. t} );
-            return {depth: uint::max(d0, d1)+1u, count: c0+c1+1u };
-        }
-        leaf(_, _) => {
-            return { depth:1u, count:1u };
-        }
-    }
+pub fn count_kd_tree_nodes( t: &kd_tree ) -> {depth:uint, count:uint} {
+	count_kd_tree_nodes_( t.root, t.nodes )
+}
+
+fn count_kd_tree_nodes_( root: uint, nodes: &[kd_tree_node]) -> {depth:uint, count:uint} {
+	match nodes[root] {
+	node(_,_,r) => {
+	let {depth:d0,count:c0} = count_kd_tree_nodes_( root+1u, nodes);
+	let {depth:d1,count:c1} = count_kd_tree_nodes_( (r as uint), nodes);
+	{depth: uint::max(d0,d1)+1u, count: c0+c1+1u }
+	}
+	leaf(_,_) => {depth:1u, count:1u}
+	}
 }
 
 pub fn read_mesh(fname: &str) -> mesh {
-    io::print("Reading model file...");
-    let polys = read_polysoup( fname );
+	io::print("Reading model file...");
+	let polys = read_polysoup( fname );
 
-    io::print("Building kd-tree... ");
+	io::print("Building kd-tree... ");
 
-    // just create a vector of 0..N-1 as our face array
-    let max_tri_ix = vec::len(polys.indices)/3u -1u;
-   let mut faces = ~[];
-   let mut fii = 0u;
-   while fii < max_tri_ix {
-      faces.push(fii);
-      fii += 1u
-   }
+	// just create a vector of 0..N-1 as our face array
+	let max_tri_ix = vec::len(polys.indices)/3u -1u;
+	let mut faces = ~[];
+	let mut fii = 0u;
+	while fii < max_tri_ix {
+		faces.push(fii);
+		fii += 1u
+	}
+	let mut aabbmin = vec3(f32::infinity, f32::infinity, f32::infinity);
+	let mut aabbmax = vec3(f32::neg_infinity, f32::neg_infinity, f32::neg_infinity);
+	for polys.vertices.each |v| {
+		aabbmin = math3d::min(*v, aabbmin);
+		aabbmax = math3d::max(*v, aabbmax);
+	}
 
-    let mut aabbmin = vec3(f32::infinity, f32::infinity, f32::infinity);
-    let mut aabbmax = vec3(f32::neg_infinity, f32::neg_infinity, f32::neg_infinity);
-    for polys.vertices.each |v| {
-        aabbmin = math3d::min(*v, aabbmin);
-        aabbmax = math3d::max(*v, aabbmax);
-    }
+	let downscale = 1.0f32 / math3d::length(sub(aabbmax,aabbmin));
+	let offset = scale(add(aabbmin, aabbmax), 0.5f32);
 
-    let downscale = 1.0f32 / math3d::length(sub(aabbmax,aabbmin));
-    let offset = scale(add(aabbmin, aabbmax), 0.5f32);
+	let mut transformed_verts = ~[];
 
-    let mut transformed_verts = ~[];
+	for polys.vertices.each |v| {
+		transformed_verts.push(scale(sub(*v, offset), downscale));
+	}
 
-
-    for polys.vertices.each |v| {
-        transformed_verts.push(scale(sub(*v, offset), downscale));
-    }
-
-    aabbmin = scale(sub(aabbmin, offset), downscale);
-    aabbmax = scale(sub(aabbmax, offset), downscale);
-
-    // de-mux vertices for easier access later
-    let mut xdists = ~[];
-    let mut ydists = ~[];
-    let mut zdists = ~[];
-
-    for transformed_verts.each |v| {
-        xdists.push(v.x);
-        ydists.push(v.y);
-        zdists.push(v.z);
-    }
-
-    let mut nodes = ~[];
-    let mut new_indices = ~[];
-    let rootnode  = build_kd_tree(
-                        &mut nodes,
-                        &mut new_indices,
-                        100u,
-                        xdists,
-                        ydists,
-                        zdists,
-                        aabbmin,
-                        aabbmax,
-                        polys.indices,
-                        faces);
-
-    { polys: {vertices: transformed_verts, indices: new_indices, .. polys}, kd_tree: { nodes: nodes, root: rootnode } , bounding_box: {min: aabbmin, max: aabbmax} }
+	aabbmin = scale(sub(aabbmin, offset), downscale);
+	aabbmax = scale(sub(aabbmax, offset), downscale);
+	// de-mux vertices for easier access later
+	let mut xdists = ~[];
+	let mut ydists = ~[];
+	let mut zdists = ~[];
+	for transformed_verts.each |v| {
+		xdists.push(v.x);
+		ydists.push(v.y);
+		zdists.push(v.z);
+	}
+	let mut nodes = ~[];
+	let mut new_indices = ~[];
+	let rootnode  = build_kd_tree(
+						&mut nodes,
+						&mut new_indices,
+						50u,
+						xdists,
+						ydists,
+						zdists,
+						aabbmin,
+						aabbmax,
+						polys.indices,
+						faces);
+	{ polys: {vertices: move transformed_verts, indices: move new_indices, .. polys}, kd_tree: kd_tree{ root: rootnode, nodes: move nodes} , bounding_box: move {min: aabbmin, max: aabbmax} }
 
 }
 
 #[inline(always)]
 fn parse_faceindex(s: ~str) ->  uint {
 
-    // check for '/', the vertex index is the first
-    let ix_str = match str::find_char(s, '/'){
-        option::Some(slash_ix) => {
-            str::substr(s, 0u, slash_ix)
-        }
-        _ => {
-            s
-        }
-    };
-
-    option::get(&uint::from_str(ix_str))-1u
+	// check for '/', the vertex index is the first
+	let ix_str = match str::find_char(s, '/'){
+		option::Some(slash_ix) => str::substr(s, 0u, slash_ix),
+		_ => move s
+	};
+	option::get(move uint::from_str(ix_str))-1u
 }
 
 fn read_polysoup(fname: &str) -> polysoup {
-    let reader = result::get( &io::file_reader( &Path(fname) ) );
-    let mut vertices = ~[];
-    let mut indices = ~[];
+	let reader = result::get( &io::file_reader( &Path(fname) ) );
+	let mut vertices = ~[];
+	let mut indices = ~[];
 
-    let mut vert_normals : ~[vec3] = ~[];
+	let mut vert_normals : ~[vec3] = ~[];
 
-    while !reader.eof() {
-        let line : ~str = reader.read_line();
-        if str::is_empty(line) {
-            loop;
-        }
+	while !reader.eof() {
+		let line : ~str = reader.read_line();
+		if str::is_empty(line) {
+			loop;
+		}
 
-        let mut num_texcoords = 0u;
-        let tokens = str::split_char(line, ' ');
+		let mut num_texcoords = 0u;
+		let tokens = str::split_char(line, ' ');
 
-        if tokens[0] == ~"v"{
-            assert vec::len(tokens) == 4u;
-            let v = vec3(	option::get(&float::from_str(tokens[1])) as f32,
-                            option::get(&float::from_str(tokens[2])) as f32,
-                            option::get(&float::from_str(tokens[3])) as f32);
-            assert v.x != f32::NaN;
-            assert v.y != f32::NaN;
-            assert v.z != f32::NaN;
+		if tokens[0] == ~"v"{
+			assert vec::len(tokens) == 4u;
+			let v = vec3(	option::get(float::from_str(tokens[1])) as f32,
+							option::get(float::from_str(tokens[2])) as f32,
+							option::get(float::from_str(tokens[3])) as f32);
+			assert v.x != f32::NaN;
+			assert v.y != f32::NaN;
+			assert v.z != f32::NaN;
 
-            vertices += [v];
-            vert_normals += [mut vec3(0f32,0f32,0f32)];
+			vertices += [v];
+			vert_normals += [mut vec3(0f32,0f32,0f32)];
 
-        } else if tokens[0] == ~"f" {
-            if vec::len(tokens) == 4u || vec::len(tokens) == 5u {
-                let mut face_triangles = ~[];
+		} else if tokens[0] == ~"f" {
+			if vec::len(tokens) == 4u || vec::len(tokens) == 5u {
+				let mut face_triangles = ~[];
 
-                if vec::len(tokens) == 4u {
-                    let (i0,i1,i2) = (	parse_faceindex(copy tokens[1]),
-                                        parse_faceindex(copy tokens[2]),
-                                        parse_faceindex(copy tokens[3]) );
+				if vec::len(tokens) == 4u {
+					let (i0,i1,i2) = (	parse_faceindex(copy tokens[1]),
+										parse_faceindex(copy tokens[2]),
+										parse_faceindex(copy tokens[3]) );
 
-                    face_triangles.push((i0, i1, i2));
-                } else {
-                    assert vec::len(tokens) == 5u;
-                    // quad, triangulate
-                    let (i0,i1,i2,i3) = (	parse_faceindex(copy tokens[1]),
-                                            parse_faceindex(copy tokens[2]),
-                                            parse_faceindex(copy tokens[3]),
-                                            parse_faceindex(copy tokens[4]) );
+					face_triangles.push((i0, i1, i2));
+				} else {
+					assert vec::len(tokens) == 5u;
+					// quad, triangulate
+					let (i0,i1,i2,i3) = (	parse_faceindex(copy tokens[1]),
+											parse_faceindex(copy tokens[2]),
+											parse_faceindex(copy tokens[3]),
+											parse_faceindex(copy tokens[4]) );
 
-                    face_triangles.push((i0,i1,i2));
-                    face_triangles.push((i0,i2,i3));
-                }
+					face_triangles.push((i0,i1,i2));
+					face_triangles.push((i0,i2,i3));
+				}
 
-                for face_triangles.each |t| {
-                    let (i0,i1,i2) = *t;
+				for face_triangles.each |t| {
+					let (i0,i1,i2) = *t;
 
-                    indices += [i0,i1,i2];
+					indices += [i0,i1,i2];
 
-                    let e1 = math3d::sub(vertices[i1], vertices[i0]);
-                    let e2 = math3d::sub(vertices[i2], vertices[i0]);
-                    let n = normalized(cross(e1,e2));
+					let e1 = math3d::sub(vertices[i1], vertices[i0]);
+					let e2 = math3d::sub(vertices[i2], vertices[i0]);
+					let n = normalized(cross(e1,e2));
 
-                    vert_normals[i0] = math3d::add( vert_normals[i0], n );
-                    vert_normals[i1] = math3d::add( vert_normals[i1], n );
-                    vert_normals[i2] = math3d::add( vert_normals[i2], n );
-                }
-            } else {
-                io::println(#fmt("Polygon with %u vertices found. Ignored. Currently rustray only supports 4 vertices", vec::len(tokens) - 1u));
-            }
-        } else if tokens[0] == ~"vt" {
-            num_texcoords += 1u;
-        } else if tokens[0] != ~"#" {
-            io::println(#fmt("Unrecognized line in .obj file: %s", line));
-        }
+					vert_normals[i0] = math3d::add( vert_normals[i0], n );
+					vert_normals[i1] = math3d::add( vert_normals[i1], n );
+					vert_normals[i2] = math3d::add( vert_normals[i2], n );
+				}
+			} else {
+				io::println(fmt!("Polygon with %u vertices found. Ignored. Currently rustray only supports 4 vertices", vec::len(tokens) - 1u));
+			}
+		} else if tokens[0] == ~"vt" {
+			num_texcoords += 1u;
+		} else if tokens[0] != ~"#" {
+			io::println(fmt!("Unrecognized line in .obj file: %s", line));
+		}
 
-        if num_texcoords > 0u {
-            io::println(#fmt("%u texture coordinates ignored", num_texcoords));
-        }
-    }
+		if num_texcoords > 0u {
+			io::println(fmt!("%u texture coordinates ignored", num_texcoords));
+		}
+	}
 
-    //for v in vert_normals {
-    //	v = normalized(v);
-    //}
+	//for v in vert_normals {
+	//	v = normalized(v);
+	//}
 
-    return {	vertices: vertices,
-            indices: indices,
-            normals: vec::map( vert_normals, |v| normalized(*v) ) };
+	return {	vertices: move vertices,
+			indices: move indices,
+			normals: vec::map( vert_normals, |v| normalized(*v) ) };
 }
