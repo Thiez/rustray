b534f7a1cc3585451e7a792049a4cbd944d6a1cb
Matthijs Hofstra <thiezz@gmail.com>
2014-04-02 18:31:10 +0200

    Modernized concurrent.rs

--- concurrent.rs
+++ concurrent.rs
@@ -1,41 +1,41 @@
-extern mod extra;
+extern crate sync;
 
-use std::*;
-use extra::future::{Future,from_port};
+use std::{comm};
+use sync::Future;
 
 struct ConcurrentCalc<T,U> {
-    priv chan: Chan<(T,~fn(T)->U,comm::ChanOne<U>)>
+  sender: comm::Sender<(T,proc(T)->U,comm::Sender<U>)>
 }
 
 impl<T:Send, U: Send> ConcurrentCalc<T,U> {
-    pub fn new() -> ConcurrentCalc<T,U> {
-        let (p, c) = comm::stream();
-        do spawn {
-            loop {
-                match p.try_recv() {
-                    Some(message) => {
-                        let (data,f,chan): (T,~fn(T)->U,comm::ChanOne<U>) = message;
-                        chan.send(f(data))
-                    }
-                    None => break
-                };
-            }
+  pub fn new() -> ConcurrentCalc<T,U> {
+    let (send, recv) = comm::channel();
+    spawn(proc() {
+      loop {
+        match recv.recv_opt() {
+          Some(message) => {
+            let (data,f,s): (T,proc(T)->U,comm::Sender<U>) = message;
+            s.send(f(data))
+          },
+          None => break
         }
-        ConcurrentCalc{ chan: c }
-    }
-    pub fn calculate(&mut self, data: T, f: ~fn(T)->U) -> Future<U> {
-        let (p, c) = comm::oneshot();
-        self.chan.send( (data,f,c) );
-        from_port(p)
-    }
+      }
+    });
+    ConcurrentCalc{ sender: send }
+  }
+  pub fn calculate(&mut self, data: T, f: proc(T)->U) -> Future<U> {
+    let (send, recv) = comm::channel();
+    self.sender.send( (data,f,send) );
+    Future::from_fn(proc(){recv.recv()})
+  }
 }
 
 #[test]
 fn testConcurrent() {
-    let mut cc: ConcurrentCalc<uint,uint> = ConcurrentCalc::new();
-    let mut future = cc.calculate(3, |x| {x+1});
-    assert!(future.get() == 4);
-    let mut future = cc.calculate(10, |x| {x-4});
-    assert!(future.get() == 6);
+  let mut cc: ConcurrentCalc<uint,uint> = ConcurrentCalc::new();
+  let mut future = cc.calculate(3, proc(x) {x+1});
+  assert!(future.get() == 4);
+  let mut future = cc.calculate(10, proc(x) {x-4});
+  assert!(future.get() == 6);
 }
 
